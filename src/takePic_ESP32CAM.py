53725140ba797bddfc826bd78ceebb684dea3409
samihamza01 <hamza@hm.edu>
2023-11-23 12:26:55 +0100

    implemented ESPCam serial interface as class

--- src/takePic_ESP32CAM.py
+++ src/takePic_ESP32CAM.py
@@ -1,65 +1,104 @@
-import cv2 as cv
 import serial
 import serial.tools.list_ports
 import struct
 from time import sleep
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import io
+from PIL import ImageFile, Image
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-PACKAGE_SIZE_BYTES = 128
+class ESPCam():
+	# attibutes
+	port = ""
+	baudrate = 115200
+	PACKAGE_SIZE_BYTES = 128
+	msgSet = {"REC": 1, "ACK": 2, "TRANSMISSION_OKAY": 3, "TRANSMISSION_FAIL": 4}
+	binImageBuffer = b''
+	ser = None
 
-msgSet = {"REC": 1, "ACK": 2, "TRANSMISSION_OKAY": 3, "TRANSMISSION_FAIL": 4}
+	# ctor
+	def __init__(self,port) -> None:
+		self.port = port
+		return
 
-ports = serial.tools.list_ports.comports()
+	def __enter__(self):
+		return self
 
-print("List of available devices:\n")
-for port, desc, hwid in sorted(ports):
-        print(f"\t{port}: {desc} [{hwid}]")
+	def __exit__(self, exc_type, exc_value, traceback):
+		self.ser.close()
+		return
 
-ser = serial.Serial("COM4",115200)
+	def connect(self):
+		self.ser = serial.Serial(self.port,115200)
+		# Trigger reset
+		self.ser.setDTR(True)
+		self.ser.setDTR(False)
+		self.ser.setRTS(False)
+		sleep(0.1)
+		self.ser.flushInput()
+		self.ser.flushOutput()
+		sleep(1)
+		return
+	
+	def recordImage(self):
+		# Send the record image command
+		msg = struct.pack("B",self.msgSet["REC"])
+		self.ser.write(msg)
 
-# Trigger reset
-ser.setDTR(True)
-ser.setDTR(False)
-ser.setRTS(False)
-sleep(0.1)
-ser.flushInput()
-ser.flushOutput()
-sleep(1)
+		# Wait for information struct
+		while self.ser.inWaiting() < 16:
+			pass
 
-# Send the record image command
-msg = struct.pack("B",msgSet["REC"])
-ser.write(msg)
-print(msg)
-# Wait for information struct
-while ser.inWaiting() < 16:
-	pass
+		rxBytes = self.ser.read(16)
+		(width, height, format, length) = struct.unpack("<LLiL",rxBytes)
 
-rxBytes = ser.read(16)
-(width, height, format, length) = struct.unpack("<LLiL",rxBytes)
+		# Send ACK message
+		msg = struct.pack("<B",self.msgSet["ACK"])
+		self.ser.write(msg)
 
-# Send ACK message
-msg = struct.pack("<B",msgSet["ACK"])
-ser.write(msg)
+		# recieve image in packages of PACKAGE_SIZE_BYTES bytes, so the recieve buffer will never overflow
+		self.binImageBuffer = b''
+		for i in range(0,int(length/self.PACKAGE_SIZE_BYTES)):
+			while self.ser.inWaiting() < self.PACKAGE_SIZE_BYTES:
+				pass
+			self.binImageBuffer = self.binImageBuffer + self.ser.read(self.PACKAGE_SIZE_BYTES)
 
-# recieve image in packages of PACKAGE_SIZE_BYTES bytes, so the recieve buffer will never overflow
-imageBytes = b''
-for i in range(0,int(length/PACKAGE_SIZE_BYTES)):
-	while ser.inWaiting() < PACKAGE_SIZE_BYTES:
-		pass
-	imageBytes = imageBytes + ser.read(PACKAGE_SIZE_BYTES)
+			# Send ACK message
+			msg = struct.pack("<B",self.msgSet["ACK"])
+			self.ser.write(msg)
 
-	# Send ACK message
-	msg = struct.pack("<B",msgSet["ACK"])
-	ser.write(msg)
+		while self.ser.inWaiting() < length%self.PACKAGE_SIZE_BYTES:
+			pass
+		self.binImageBuffer = self.binImageBuffer + self.ser.read(length%self.PACKAGE_SIZE_BYTES)
 
-while ser.inWaiting() < length%PACKAGE_SIZE_BYTES:
-	pass
-imageBytes = imageBytes + ser.read(length%PACKAGE_SIZE_BYTES)
+		# Send ACK message
+		msg = struct.pack("<B",self.msgSet["ACK"])
+		self.ser.write(msg)
+		return
 
-# Send ACK message
-msg = struct.pack("<B",msgSet["ACK"])
-ser.write(msg)
+	def displayImageBuffer(self):
+		fp = io.BytesIO(bytearray(self.binImageBuffer))
+		with fp:
+			img = Image.open(fp=fp)
+			img.show()
 
-ser.close()
+	def storeImage(self,path):
+		with open(path,"wb") as binaryFile:
+			binaryFile.write(self.binImageBuffer)
 
-with open("test.jpeg","wb") as binaryFile:
-	binaryFile.write(imageBytes)
+	def disconnect(self):
+		self.ser.close()
+
+if __name__ == "__main__":
+	try:
+		cam = ESPCam("COM4")
+		cam.connect()
+		cam.recordImage()
+		cam.displayImageBuffer()
+		cam.storeImage("test.jpeg")
+		cam.disconnect()
+	except:
+		print("Error Occured: Disconnecting from Camera...")
+		cam.disconnect()
+		print("End Programm")
