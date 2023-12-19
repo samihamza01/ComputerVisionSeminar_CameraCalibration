7aae02a9268517e07bb0ee967ef6863ed823db76
samihamza01 <hamza@hm.edu>
2023-12-19 16:32:47 +0100

    Added a calibratoin comparison script

--- src/calibration_test.py
+++ src/calibration_test.py
@@ -3,6 +3,7 @@ import cv2 as cv
 import os
 import camera_model as cm
 import matplotlib.pyplot as plt
+import time
 
 # Checkerboard square size
 SQUARE_SIZE = 29 # mm
@@ -29,26 +30,50 @@ for fname in images:
 		# Draw and display the corners
 		cv.drawChessboardCorners(img, (9,6), corners2, ret)
 		img = cv.resize(img,(int(img.shape[1]*0.4),int(img.shape[0]*0.4)))
-		cv.imshow('img', img)
-		cv.waitKey(0)
+		#cv.imshow('img', img)
+		#cv.waitKey(0)
 cv.destroyAllWindows()
 
 
 # Calibrate with OpenCV
-ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-print(tvecs)
+RMSErrorCV, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 # Calibrate with own implementation
 cam = cm.CameraModel()
-cam.calibrate(objpoints,imgpoints,gray.shape[0:1])
+RMSError, rVecs, tVecs = cam.calibrate(objpoints,imgpoints,gray.shape[0:1])
 cam.save_cam_to_json("test.json")
 
 print("Comparison OpenCV and Own Implementation")
+print("RMS-Reprojection-Error")
+print(f"OpenCV:\t\t{RMSErrorCV}")
+print(f"OwnImpl:\t{RMSError}")
 print("Camera Matrix")
-print(f"OpenCV:\t{mtx}")
+print(f"OpenCV:\t\t{mtx}")
 print(f"OwnImpl:\t{cam.cameraMat}")
 print("Distortion Coefficients")
 print(f"OpenCV:\t\tk1={dist[0][0]}, k2={dist[0][1]}, k3={dist[0][4]}, p1={dist[0][2]}, p2={dist[0][3]}")
 print(f"OwnImpl:\tk1={cam.distortionVec[0]}, k2={cam.distortionVec[1]}, k3={cam.distortionVec[2]}, p1={cam.distortionVec[3]}, p2={cam.distortionVec[4]}")
+
+
+def comp_params(object_points, image_points, camera_matrix = None, r_vecs = None, t_vecs = None):
+	# Calibrate with OpenCV
+	print("Starting calibration with OpenCV:")
+	startTime = time.time()
+	RMSErrorCV, mtxCV, distCV, rvecsCV, tvecsCV = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], camera_matrix, None, r_vecs, t_vecs)
+	stopTime = time.time()
+	print(f"Calibration took {stopTime-startTime} s.")
+
+	# Calibrate with own implementation
+	print("Starting calibration with OpenCV:")
+	startTime = time.time()
+	cam = cm.CameraModel()
+	RMSError, rVecs, tVecs = cam.calibrate(objpoints,imgpoints,gray.shape[0:1])
+	stopTime = time.time()
+	print(f"Calibration took {stopTime-startTime} s.")
+
+
+
+
+
 
 
 
--- src/camera_model.py
+++ src/camera_model.py
@@ -124,75 +124,18 @@ class CameraModel():
 		v_rot = np.matmul(rotMat, vector)
 		return v_rot
 
-	def calibrate(self, objectPoints: tp.List[tp.List[npt.ArrayLike]], imagePoints: tp.List[tp.List[npt.ArrayLike]], imageSize: tp.Tuple[int,int], verbose: tp.Optional[bool] = False) -> None:
-		levMar = lm.LevenbergMarquardtOptimizer(maxIterations=40,parameterStepThr=1e-5,gradientThr=1e-5)
-		# TODO: add homographie part for initial values-----
-		# get inial parameters via homographie
-		# for now hardcoded data of matlab dataset
-		numViews = len(objectPoints)
-		parameterVec = np.zeros((9+6*numViews,))
-		parameterVec[0] = 4.5e3
-		parameterVec[1] = 4.6e3
-		parameterVec[2] = 1.4e3
-		parameterVec[3] = 1.1e3
-		# rotation values
-		parameterVec[9] = -0.7543
-		parameterVec[10] = -0.2778
-		parameterVec[11] = -0.1074
-		parameterVec[12] = -0.8174
-		parameterVec[13] = -0.1010
-		parameterVec[14] = -0.0660
-		parameterVec[15] = -0.7192
-		parameterVec[16] = -0.0772
-		parameterVec[17] = -0.0471
-		parameterVec[18] = -0.7124
-		parameterVec[19] = -0.1927
-		parameterVec[20] = -0.0487
-		parameterVec[21] = -0.8465
-		parameterVec[22] = -0.2002
-		parameterVec[23] = -0.0768
-		parameterVec[24] = -0.7503
-		parameterVec[25] = -0.1890
-		parameterVec[26] = -0.4599
-		parameterVec[27] = -0.6712
-		parameterVec[28] = -0.4151
-		parameterVec[29] = -1.2874
-		parameterVec[30] = -0.6248
-		parameterVec[31] = -0.5495
-		parameterVec[32] = -1.5243
-		parameterVec[33] = -0.7686
-		parameterVec[34] = -0.3634
-		parameterVec[35] = -1.4283
-
-		# translation values
-		parameterVec[36] = -180.06
-		parameterVec[37] = -57.893
-		parameterVec[38] =  786.23
-		parameterVec[39] = -97.378
-		parameterVec[40] = -40.246	
-		parameterVec[41] =  835.66
-		parameterVec[42] = -61.589
-		parameterVec[43] = -141.33
-		parameterVec[44] =  825.77
-		parameterVec[45] = -123.90
-		parameterVec[46] = -148.99
-		parameterVec[47] =  831.32
-		parameterVec[48] = -124.13
-		parameterVec[49] = -28.369
-		parameterVec[50] =  871.66
-		parameterVec[51] = -154.31
-		parameterVec[52] = -71.503
-		parameterVec[53] =  822.46
-		parameterVec[54] = -120.83
-		parameterVec[55] =  26.938
-		parameterVec[56] =  695.35
-		parameterVec[57] = -139.21
-		parameterVec[58] =  43.420
-		parameterVec[59] =  686.92
-		parameterVec[60] = -28.554
-		parameterVec[61] =  31.035
-		parameterVec[62] =  724.86
-		# --------------------------------------------------
+	def calibrate(self, objectPoints: tp.List[tp.List[npt.ArrayLike]], imagePoints: tp.List[tp.List[npt.ArrayLike]], imageSize: tp.Tuple[int,int], rVecs: tp.Optional[tp.List[npt.ArrayLike]] = None, tVecs: tp.Optional[tp.List[npt.ArrayLike]] = None, cameraMatrix: tp.Optional[npt.ArrayLike] = None, verbose: tp.Optional[bool] = False) -> tp.Tuple[float,tp.List[npt.ArrayLike],tp.List[npt.ArrayLike]]:
+		# Perform some input parameter checks
+		if rVecs == None and tVecs == None and cameraMatrix == None:
+			estimateInitGuess = True
+		elif rVecs != None and tVecs != None and cameraMatrix != None:
+			estimateInitGuess = False
+			if len(objectPoints) != len(rVecs) or len(objectPoints) != len(tVecs):
+				raise ValueError("Translation and rotation vectors must have same length as the number of views (images).")
+			if cameraMatrix.shape != (3,3):
+				raise ValueError("Camera matrix must be of shape (3,3)")
+		else:
+			raise ValueError("If giving an initial guess, the camera matrix, rotation vectors and translation vectors must be given.")
 
 		# get calibration data structure and length
 		self.calibrationDataStructure = []
@@ -200,6 +143,89 @@ class CameraModel():
 		for view in objectPoints:
 			self.calibrationDataStructure.append(len(view))
 			length += len(view)
+		
+		if estimateInitGuess:
+			# TODO: add homographie part for initial values-----
+			# get inial parameters via homographie
+			# for now hardcoded data of matlab dataset
+			numViews = len(objectPoints)
+			parameterVec = np.zeros((9+6*numViews,))
+			parameterVec[0] = 4.5e3
+			parameterVec[1] = 4.6e3
+			parameterVec[2] = 1.4e3
+			parameterVec[3] = 1.1e3
+			# rotation values
+			parameterVec[9] = -0.7543
+			parameterVec[10] = -0.2778
+			parameterVec[11] = -0.1074
+			parameterVec[12] = -0.8174
+			parameterVec[13] = -0.1010
+			parameterVec[14] = -0.0660
+			parameterVec[15] = -0.7192
+			parameterVec[16] = -0.0772
+			parameterVec[17] = -0.0471
+			parameterVec[18] = -0.7124
+			parameterVec[19] = -0.1927
+			parameterVec[20] = -0.0487
+			parameterVec[21] = -0.8465
+			parameterVec[22] = -0.2002
+			parameterVec[23] = -0.0768
+			parameterVec[24] = -0.7503
+			parameterVec[25] = -0.1890
+			parameterVec[26] = -0.4599
+			parameterVec[27] = -0.6712
+			parameterVec[28] = -0.4151
+			parameterVec[29] = -1.2874
+			parameterVec[30] = -0.6248
+			parameterVec[31] = -0.5495
+			parameterVec[32] = -1.5243
+			parameterVec[33] = -0.7686
+			parameterVec[34] = -0.3634
+			parameterVec[35] = -1.4283
+
+			# translation values
+			parameterVec[36] = -180.06
+			parameterVec[37] = -57.893
+			parameterVec[38] =  786.23
+			parameterVec[39] = -97.378
+			parameterVec[40] = -40.246	
+			parameterVec[41] =  835.66
+			parameterVec[42] = -61.589
+			parameterVec[43] = -141.33
+			parameterVec[44] =  825.77
+			parameterVec[45] = -123.90
+			parameterVec[46] = -148.99
+			parameterVec[47] =  831.32
+			parameterVec[48] = -124.13
+			parameterVec[49] = -28.369
+			parameterVec[50] =  871.66
+			parameterVec[51] = -154.31
+			parameterVec[52] = -71.503
+			parameterVec[53] =  822.46
+			parameterVec[54] = -120.83
+			parameterVec[55] =  26.938
+			parameterVec[56] =  695.35
+			parameterVec[57] = -139.21
+			parameterVec[58] =  43.420
+			parameterVec[59] =  686.92
+			parameterVec[60] = -28.554
+			parameterVec[61] =  31.035
+			parameterVec[62] =  724.86
+		else:
+			parameterVec[0] = cameraMatrix[0][0]
+			parameterVec[1] = cameraMatrix[1][1]
+			parameterVec[2] = cameraMatrix[0][2]
+			parameterVec[3] = cameraMatrix[1][2]
+			
+			t_offset = len(objectPoints)*3
+			for viewIdx in range(0,len(objectPoints)):
+				parameterVec[9+viewIdx*3:12+viewIdx*3] = rVecs[viewIdx][:]
+				parameterVec[t_offset+viewIdx*3:t_offset+3+viewIdx*3] = tVecs[viewIdx][:]
+		# --------------------------------------------------
+		
+		# Optimization ----------------------------------------
+		# create optimzer
+		levMar = lm.LevenbergMarquardtOptimizer(maxIterations=40,parameterStepThr=1e-5,gradientThr=1e-5)
 
 		# serialize the object and image data
 		objectPointsSer = np.zeros((length,3))
@@ -211,35 +237,45 @@ class CameraModel():
 				imagePointsSer[serCounter][:] = imagePoints[viewIdx][pointIdx]
 				serCounter += 1
 
-		# start optimization
 		# call _residual_function with initCall=True befor optimization
 		self._residual_function(parameterVec,objectPointsSer,imagePointsSer,initCall=True)
-		optimalParams, conv, error, meanResidual, _, _, iteration, squareErrorHist = levMar.optimize(self._residual_function,parameterVec,objectPointsSer,imagePointsSer)
+		optimalParams, conv, error, _, _, _, iteration, squareErrorHist = levMar.optimize(self._residual_function,parameterVec,objectPointsSer,imagePointsSer)
+		# -----------------------------------------------------
+
 		self.cameraMat = np.array([	[optimalParams[0],0,optimalParams[2]],
 									[0,optimalParams[1],optimalParams[3]],
 									[0,0,1]])
-
-		# show result and store camera data
 		self.distortionVec = optimalParams[4:9]
 		self.imageSize = imageSize
-		if conv == 0:
-			print(f"Convergence in gradient after {iteration} iterations.")
-		elif conv == 1:
-			print(f"Convergence in parameters after {iteration} iterations.")
-		else:
-			print(f"No convergence reached after max iterations [{iteration}]!")
-		print(f"Final squared error: {error}")
-		print(f"Mean residuals: {meanResidual}")
-		# plot history
-		ax = plt.subplot(111)
-		ax.plot(np.linspace(0,iteration,iteration+1),squareErrorHist)
-		ax.set_title("Convergence Plot")
-		ax.set_xlabel("Iteration")
-		ax.set_ylabel("Square Error")
-		ax.set_xlim(0,iteration)
-		ax.set_ylim(bottom=0)
-		plt.show()
-		return
+		RMSError = np.sqrt(error)/length
+
+		# get rVecs and tVecs
+		rVecs = []
+		tVecs = []
+		t_offset = len(objectPoints)*3+9
+		for viewIdx in range(0,len(objectPoints)):
+			rVecs.append(parameterVec[9+viewIdx*3:12+viewIdx*3])
+			tVecs.append(parameterVec[t_offset+viewIdx*3:t_offset+3+viewIdx*3])
+
+		if verbose:
+			# show result
+			if conv == 0:
+				print(f"Convergence in gradient after {iteration} iterations.")
+			elif conv == 1:
+				print(f"Convergence in parameters after {iteration} iterations.")
+			else:
+				print(f"No convergence reached after max iterations [{iteration}]!")
+			print(f"Final RMS reprojection error: {RMSError}")
+			# plot history
+			ax = plt.subplot(111)
+			ax.plot(np.linspace(0,iteration,iteration+1),squareErrorHist)
+			ax.set_title("Convergence Plot")
+			ax.set_xlabel("Iteration")
+			ax.set_ylabel("Square Error")
+			ax.set_xlim(0,iteration)
+			ax.set_ylim(bottom=0)
+			plt.show()
+		return (RMSError, rVecs, tVecs)
 	
 	def _world_2_image(self, parameterVec: npt.ArrayLike, objectPoint: npt.ArrayLike) -> npt.ArrayLike:
 		"""Helper function to transform an object/world point to the image plane of the camera.
